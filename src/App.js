12b590c3291e5e38a93d2756320db9808a989416
Abdul Hadi <ahadiwasti@gmail.com>
2023-05-18 14:31:34 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import PropTypes from 'prop-types'
 import pokemons from './pokemon.json'
+import React from 'react';
 
-const PokemonRow = ({pokemon})=>(
+const PokemonRow = ({pokemon, onSelect})=>(
   <tr>
   <td>{pokemon.name.english}</td>
   <td>{pokemon.type.join(', ')}</td>
+  <td>
+    <button onClick={()=>onSelect(pokemon)}>Select</button>
+  </td>
   </tr>
   );
 PokemonRow.propTypes={
   pokemon : PropTypes.shape({
     name : PropTypes.string,
     type: PropTypes.arrayOf(PropTypes.string)
-  })
+  }),
+  onSelect: PropTypes.func
 }
 
 function App() {
+  const [filterValue, filterSet] = React.useState("")
+  const [selectedItem, setItem] = React.useState("")
+
   return (
     <>
     <div style={{
@@ -24,7 +32,17 @@ function App() {
       
     }}>
      <div className='title'>Pokemon Search</div>
-     <table width="100%">
+     
+     <div style={{
+      display:"grid",
+      gridTemplateColumns: '70% 30%',
+      gridColumnsGap: "1rem"
+     }}>
+      <div>
+      <input value={filterValue} 
+     onChange={(evt)=>filterSet(evt.target.value)}
+     />
+       <table width="100%">
       <thead>
         <tr>
         <th>Name</th>
@@ -33,15 +51,23 @@ function App() {
         
       </thead>
       <tbody>
-        {pokemons.slice(0,20).map(pokemon=>(
-           <PokemonRow pokemon={pokemon} key={pokemon.id}/>
+        {pokemons.slice(0,20).filter((pokemon)=> pokemon.name.english.toLowerCase().includes(filterValue.toLowerCase())).map(pokemon=>(
+           <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon)=> setItem(pokemon.name.english)}/>
         ))}
-       
-      
       </tbody>
      </table>
+      </div>
+     
      </div>
-    </>
+          <div>
+          {selectedItem && (
+            <div>
+              <h1>{selectedItem}</h1>
+            </div>
+          )}
+          </div>
+     </div>
+    </>  
   );
 }
 
